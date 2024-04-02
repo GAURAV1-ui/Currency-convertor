5761b69574b9039552fa2e0131d53061456978ad
Gaurav <iamGaurav201@gmail.com>
2024-04-03 02:01:29 +0530

    Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,9 @@ import './App.css'
 
 function App() {
 
-  let counter = 15;
-
-  const addValue = () => {
-    console.log(counter);
-    counter++;
-  }
-
-
   return (
     <>
-      <h1>Chai aur react {counter}</h1>
-      <h2>Counter Value</h2>
-      <button onClick={addValue}>Button</button>
+  <h1>Hello</h1>
     </>
   )
 }
